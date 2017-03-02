910c64c14a018d0169946e6b6de5005b006b20ad
Tyrone Trevorrow <tyrone@sudeium.com>
2017-03-02 16:05:42 +1100

    Adds problem description to remaining programs

--- 0008.rb
+++ 0008.rb
@@ -1,3 +1,6 @@
+# The four adjacent digits in the 1000-digit number that have the greatest
+# product are 9 × 9 × 8 × 9 = 5832.
+
 DIGITS = <<EOF
 73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
@@ -20,6 +23,9 @@ DIGITS = <<EOF
 05886116467109405077541002256983155200055935729725
 71636269561882670428252483600823257530420752963450
 EOF
+
+# Find the thirteen adjacent digits in the 1000-digit number that have the
+# greatest product. What is the value of this product?
 
 def strsum(str)
   total = 1
