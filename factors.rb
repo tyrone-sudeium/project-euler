5af39a46ee4d344b2e215ac7631309d4c73f05e3
Tyrone Trevorrow <tyrone@sudeium.com>
2017-03-03 11:37:17 +1100

    Refactors factors

--- factors.rb
+++ factors.rb
@@ -1,17 +1,14 @@
 # Given a number, this returns an enumerator over its factors.
 def factors(n)
+  return [] if n.nil?
   Enumerator.new do |enum|
-    i = 2
     large_factors = []
-    while n > i
+    1.upto(Math.sqrt(n).to_i).each do |i|
       if n % i == 0
-        enum.yield(i)
-        n = n / i
-        large_factors << n if n > i
+        enum.yield i if n % i == 0
+        large_factors << n / i unless i == n / i
       end
-      i = i + 1
     end
-
     large_factors.reverse.each do |f|
       enum.yield(f)
     end
